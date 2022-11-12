9b83a311f416b973a91c978457387c0bd65055e3
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-11-12 23:15:22 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    JWT_SECRET : "shkhs23kkfdfjdfjdfhdh",
-    DB_HOST : "localhost",
-    DB_PORT : 3306,
-    DB : "petdb",
-    DB_USER : "root",
-    DB_PASSWORD : "Satyam20@"
-}
