9601f5d5225e5ed42b615e43f9e444127ccb5e25
Deepak <trojanhorsedc@gmail.com>
2022-12-11 01:42:46 +0530

    DB update

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
     JWT_SECRET : "shkhs23kkfdfjdfjdfhdh",
     DB_HOST : "103.174.242.29",
     DB_PORT : 3307,
-    DB : "hostname",
+    DB : "Satyam",
     DB_USER : "root",
     DB_PASSWORD : "satyam123"
 }
