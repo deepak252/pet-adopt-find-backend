88f5ec3c4129b2485a405de89ef0a092527cf3fd
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-12-10 09:53:45 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    JWT_SECRET : "shkhs23kkfdfjdfjdfhdh",
-    DB_HOST : "sql9.freesqldatabase.com",
-    DB_PORT : 3306,
-    DB : "sql9563932",
-    DB_USER : "sql9563932",
-    DB_PASSWORD : "YCDTirUkBG"
-}
