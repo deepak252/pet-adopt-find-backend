bec6c2933cb4b833dba2bd12c1a5c2adf1aa9c51
Satyam Chaurasia <satyamchaurasia2011@gmail.com>
2022-12-17 23:18:13 +0530

    add and edit pet done

--- controllers/petController.js
+++ controllers/petController.js
@@ -5,10 +5,10 @@ const { insertAddress } = require("./addressController");
 const {query} = require('../db');
 const { successMessage, errorMessage } = require("../utils/responseUtils");
 
-const createPet = async(req, res) => {
+module.exports.createPet = async(req, res) => {
     try {
-        const { userId, petName, breed, age, photos, category, petStatus,
-        addressLine, city, state, pincode, coordinates } = req.body;
+        const { userId, petName, petInfo, breed, age, photos, category, petStatus,
+        addressLine, city, state, pincode, coordinates, gender } = req.body;
         const addressId = await insertAddress(addressLine, city, state, pincode, coordinates);
         //console.log({addressId})
        const createPetTableQuery = `CREATE TABLE IF NOT EXISTS pets(
@@ -17,16 +17,18 @@ const createPet = async(req, res) => {
        addressId int(11),
        FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
        FOREIGN KEY (addressId) REFERENCES address(addressId) ON DELETE CASCADE,
-       petName varchar(25),
+       petName varchar(25), 
+       petInfo varchar(255),
        breed varchar(25),
-       age int(8),
+       age varchar(8),
        photos varchar(255),
        category varchar(25),
+       gender varchar(10),
        petStatus varchar(10),
        createdAt varchar(100)
        );
        `
-     const newPost = new Pet(userId, petName, breed, age, addressId, photos, category, petStatus, new Date()) ;
+     const newPost = new Pet(userId, petName, petInfo, breed, age, addressId, photos, category, gender, petStatus, new Date()) ;
       const createPostQuery = `
       INSERT INTO pets VALUES (NULL, ${newPost.toString()});
       `
@@ -51,7 +53,7 @@ const createPet = async(req, res) => {
     }
 }
 
-const getAllPets = async(req, res) => {
+module.exports.getAllPets = async(req, res) => {
     try {
         const adoptTypeQuery = `
             SELECT * FROM pets
@@ -67,8 +69,68 @@ const getAllPets = async(req, res) => {
     }
 }
 
+module.exports.editPet = async(req, res) => {
+    try {
+        const { petName, petInfo, breed, age, photos, category, petStatus,
+            addressLine, city, state, pincode, coordinates } = req.body;
+            //addressId of a pet for updation
+            const getPetAddressIdQuery = `
+            select addressId from pets where petId = "${req.params.petId}";
+            `
+            //get addressId
+            const [responseAdd] = await query(getPetAddressIdQuery);
+            const updateAddressQuery = `
+            update address 
+            set addressLine = "${addressLine}", city = "${city}", 
+            state = "${state}", pincode = "${pincode}", 
+            coordinates = "${coordinates}" 
+            where addressId = "${responseAdd.addressId}";
+            `
+            const updatePetQuery = `
+            update pets 
+            set petName="${petName}", petInfo="${petInfo}", breed="${breed}", age="${age}",
+            photos="${photos}", category="${category}", petStatus="${petStatus}"
+            where petId="${req.params.petId}";
+            `
+            const result = await query(updatePetQuery);
+            await query(updateAddressQuery);
+            return res.json(successMessage(
+                result
+           ))
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
+}
 
-module.exports = {
-    createPet,
-    getAllPets
+module.exports.deletePetPost = async(req, res) => {
+    try {
+        const getPetAddressIdQuery = `
+        select userId,addressId from pets where petId = "${req.params.petId}";
+        `
+        const [responseAdd] = await query(getPetAddressIdQuery);
+        const getUserQuery = `
+        select uploadPetsId from users where userId = "${responseAdd.userId}";
+        `
+        const [responseUser] = await query(getUserQuery);
+        const newuploadPetsId = responseUser.uploadPetsId.split(',').filter(idx => idx != req.params.petId);
+        
+        const deletePetQuery = `
+        delete from pets where petId = "${req.params.petId}";
+        `
+        const deleteAddressQuery = `
+         delete from address where addressId = "${responseAdd.addressId}";
+        `
+
+        const removePetIdQuery = `
+        update users set uploadPetsId="${newuploadPetsId}" where userId="${responseAdd.userId}"
+        `
+        const result = await query(deletePetQuery);
+        await query(deleteAddressQuery);
+        await query(removePetIdQuery);
+        return res.json(successMessage(
+            result
+       ))
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
 }
--- routes/petRoute.js
+++ routes/petRoute.js
@@ -4,10 +4,15 @@ const petController = require("../controllers/petController");
 const { auth } = require("../middlewares/auth");
 
 //create a post for adoption of a Pet
-route.post('/pet/createPet', auth, petController.createPet);
+route.post('/pet/create', auth, petController.createPet);
 
 //get All pets data
 route.get('/pet/getAllPets', auth, petController.getAllPets);
 
+//Edit pet post
+route.put('/pet/editPet/:petId', auth, petController.editPet)
+
+//delete pet post
+route.delete('/pet/delete/:petId', auth, petController.deletePetPost);
 module.exports = route;
 
