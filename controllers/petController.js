ad99d0e9b14e5568d46f65a9fdfdcd19dccf5251
Deepak <trojanhorsedc@gmail.com>
2023-01-10 01:15:26 +0530

    edit pet fix

--- controllers/petController.js
+++ controllers/petController.js
@@ -122,10 +122,10 @@ module.exports.editPet = async (req, res) => {
     } = req.body;
 
     //get addressId
-    const [responsePet] = await query(sqlQueries.getPets(req.params.petId));
-    await query(sqlQueries.editPetDetails(petName,petInfo,breed,age,photos,category,gender,petStatus,req.params.petId));
+    const [responsePet] = await query(sqlQueries.getPets(req.petId));
+    await query(sqlQueries.editPetDetails(petName,petInfo,breed,age,photos,category,gender,petStatus,petId));
     await query(sqlQueries.editAddress(addressLine,city,state,pincode,coordinates,responsePet.addressId));
-    const result = await query(sqlQueries.getPets([req.params.petId]));
+    const result = await query(sqlQueries.getPets([petId]));
     return res.json(successMessage(result));
   } catch (error) {
     return res.status(400).send(error.message);
